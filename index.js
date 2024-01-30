a23b1df160a46d86ece12be825f9390f59366aec
Ashesh9 <149551472+Ashesh9@users.noreply.github.com>
2024-01-29 22:11:46 -0500

    Intial Title Prompts

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-// TODO: Include packages needed for this application
-
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { of } = require("rxjs");
 
+const questions= ()=>{
+    return inquirer.prompt([
+        {
+        type:Input;
+        name : Title;
+        Message:    Please enter your Project Title;
+        }
+
+    ])
+};
+const generateREADME=({})=>
+
+
+`#${Title}
+##${Description}
+##${Table of contents}
+##${Installation}
+##${Usage}
+##${License }
+##${Contributing}
+##${Tests}
+##${Questions and Answers}
+`
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init=()=> {
+    question()
+    .then(
+        (answers)=>writeFile("README.md",generateREADME (answers)))
+    .then(()=>console.log("Successfully Generated README.md")
+    .catch((err)=>console.log("Error");    
+    )
+}
 
 // Function call to initialize app
 init();
