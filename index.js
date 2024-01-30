34a544e281a1e57909e97611f82fbd3992e782af
Ashesh9 <149551472+Ashesh9@users.noreply.github.com>
2024-01-29 22:28:11 -0500

    Removed syntax errors

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 // TODO: Create an array of questions for user input
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { of } = require("rxjs");
 
-const questions= ()=>{
-    return inquirer.prompt([
-        {
-        type: 'input ',
-        name : 'Title',
-        message: 'Please enter your Project Title :, 
-        }
-        {
-        type: 'input',
-         name :'Description',
-         message:'Please enter your Project Description:'       
-        }
-
-    ])
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input ",
+      name: "Title",
+      message: "Please enter your Project Title :",
+    },
+    {
+      type: "input",
+      name: "Description",
+      message: "Please enter your Project Description:",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Please select applicable license:",
+      choices: [
+        "MIT License",
+        "GNU General Public License (GPL)",
+        "Apache License 2.0",
+        "BSD 2-Clause License",
+        "BSD 3-Clause License",
+        "GNU Lesser General Public License (LGPL)",
+        "Mozilla Public License 2.0",
+        "Creative Commons",
+        "Unlicense",
+      ],
+    },
+  ]);
 };
-const generateREADME=({})=>
-
-
-`#${Title}
+const generateREADME = ({}) =>
+  `#${Title}${license}
 ##${Description}
-##${Table of contents}
+##${Table - contents}
 ##${Installation}
 ##${Usage}
-##${License }
+##${License}
 ##${Contributing}
 ##${Tests}
-##${Questions and Answers}
-`
+##${Questions_Answers}
+`;
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init=()=> {
-    question()
-    .then(
-        (answers)=>writeFile("README.md",generateREADME (answers)))
-    .then(()=>console.log("Successfully Generated README.md"))
-    .catch((err)=>console.log("Error");    
-    )
+const init = () => {
+  question()
+    .then((answers) => writeFile("README.md", generateREADME(answers)))
+    .then(() => console.log("Successfully Generated README.md"))
+    .catch((err) => console.log("Error"));
 };
 
 // Function call to initialize app
